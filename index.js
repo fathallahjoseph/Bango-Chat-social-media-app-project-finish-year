9eb57385ddd8d61e41df43c706ac563585280676
Joseph Fathallah <fathallahomar350@gmail.com>
2023-08-03 19:35:48 +0100

    suppaddmdf

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ mongoose.connect(process.env.Mongo)
 .then(()=>console.log('db atlas connected succéesfully!!! '));
 //connect widh db atlas
 
-app.listen(process.env.PORT||6000,(err)=>{err?console.log(err)
+app.listen(process.env.PORT||10000,(err)=>{err?console.log(err)
     :console.log(`server is running in this port"${process.env.PORT}"`)});//conected server and making
 
 app.get("/",(req,res)=>{
